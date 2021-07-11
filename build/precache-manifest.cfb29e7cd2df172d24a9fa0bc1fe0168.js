d1f84e20cbba721f8f4ee9c2e7bc632ba876ba76
samhitha98 <cdime922@gmail.com>
2021-07-12 02:18:19 +0530

    final commit

---
+++ build/precache-manifest.cfb29e7cd2df172d24a9fa0bc1fe0168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e600289956dff9fa730df43577f2b24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ede2614e56f11395215",
+    "url": "/static/css/main.73728c27.chunk.css"
+  },
+  {
+    "revision": "4bc7bce6cfacb5386637",
+    "url": "/static/js/2.96f71534.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.96f71534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ede2614e56f11395215",
+    "url": "/static/js/main.95e4dcec.chunk.js"
+  },
+  {
+    "revision": "b10d08823a4df677ba67",
+    "url": "/static/js/runtime-main.de931e19.js"
+  }
+]);
